d139ba8e3507ae4087098143ddc0e4a9b57042b1
viiiviii538 <viiiviii538@gmail.com>
2025-09-12 12:54:36 +0900

    chore: enhance setup script

--- setup.sh
+++ setup.sh
@@ -1,29 +1,27 @@
 #!/usr/bin/env bash
 set -e
 
-# Setup Python virtual environment if requirements.txt or requirements-dev.txt exists
-if [ -f "requirements.txt" ] || [ -f "requirements-dev.txt" ]; then
-    echo "Creating Python virtual environment..."
-    python3 -m venv .venv
-    # shellcheck disable=SC1091
-    source .venv/bin/activate
-    echo "✅ Virtual environment ready."
+# Setup Python virtual environment
+echo "Creating Python virtual environment..."
+python3 -m venv .venv
+# shellcheck disable=SC1091
+source .venv/bin/activate
+echo "✅ Virtual environment ready."
 
-    echo "Upgrading pip..."
-    pip install --upgrade pip
-    echo "✅ pip upgraded."
+echo "Upgrading pip..."
+pip install --upgrade pip
+echo "✅ pip upgraded."
 
-    if [ -f "requirements.txt" ]; then
-        echo "Installing Python dependencies from requirements.txt..."
-        pip install -r requirements.txt
-        echo "✅ requirements.txt installed."
-    fi
+if [ -f "requirements.txt" ]; then
+    echo "Installing Python dependencies from requirements.txt..."
+    pip install -r requirements.txt
+    echo "✅ requirements.txt installed."
+fi
 
-    if [ -f "requirements-dev.txt" ]; then
-        echo "Installing Python development dependencies from requirements-dev.txt..."
-        pip install -r requirements-dev.txt
-        echo "✅ requirements-dev.txt installed."
-    fi
+if [ -f "requirements-dev.txt" ]; then
+    echo "Installing Python development dependencies from requirements-dev.txt..."
+    pip install -r requirements-dev.txt
+    echo "✅ requirements-dev.txt installed."
 fi
 
 # Install Node dependencies if package.json exists
@@ -38,4 +36,4 @@ if [ -f "pubspec.yaml" ]; then
     flutter pub get
 fi
 
-echo "✅ Setup complete."
+echo "✅ Setup complete!"
